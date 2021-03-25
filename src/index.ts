2190465726bcf34f9ab9c009bcacdbadc454833e
Erica Phelps <erica@blackspork.com>
2021-03-25 16:47:35 -0500

    Added rxjs to dependencies Added optional getToken callback to GraphQLClient constructor

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,52 @@
+import { GraphQLClient as reqclient, gql } from 'graphql-request';
+import { pluck } from 'rxjs/operators';
+
+// Re-export for ease-of-import
+export { gql } from 'graphql-request';
+
 export class GraphQLClient {
 
-  /**
-   * Creates a GraphQL client
-   * @param endpoint URI to which to connect.
-   */
-  constructor(endpoint: string = "http://localhost:8080/graphql") {
+  private _client: reqclient = null;
+  private _endpoint: string = "";
 
+  /** Authorization bearer token */
+  public get Token(): string { return this._getToken(); }
+  public set Token(val: string) { this._getToken = () => val; }
+
+  private _getToken: () => string = () => "";
+
+  public BearerToken: string = "";
+
+
+  /**
+   * Creates a GraphQL client.
+   * @param endpoint The URI to which to connect.
+   * @param getToken A function to retrieve an authorization token, if applicable.
+   */
+  constructor(endpoint: string = "http://localhost:8080/graphql", authToken: string | (() => string) = null) {
+    this._client = new reqclient(endpoint);
+    this._endpoint = endpoint;
+
+    if (authToken && typeof (authToken) === "string")
+      this.Token = authToken;
+    else if (authToken && typeof (authToken) === "function")
+      this._getToken = authToken;
+  }
+
+
+
+  /**
+   * Low(ish)-level query wrapper.
+   * @param query The query/mutation to run.
+   * @param params Any variables to be passed to the query.
+   * @returns A promise that resolves with the results of the query.
+   */
+  public async request(query: string, params: any = {}): Promise<any> {
+    let headers: any = {};
+    // let token = localStorage.getItem("gql_auth_token");
+    let token: string = this._getToken();
+    if (token)
+      headers.authorization = `Bearer ${token}`;
+    return this._client.request(query, params, headers);
   }
 }
--- test/basic.ts
+++ test/basic.ts
@@ -1,9 +1,19 @@
 import { expect, assert, should } from 'chai';
-import { GraphQLClient } from '../src/';
+import { GraphQLClient, gql } from '../src/';
 
-describe("Basic Tests", () => {
+describe("Basic Requests", () => {
   let client: GraphQLClient = null;
   before("Creates library", () => {
     client = new GraphQLClient();
+  });
+
+  describe("Unauthenticated", () => {
+    it("Retrieves Clusters", async () => {
+      let res = await client.request(gql`{ Cluster { id, Name }}`);
+      assert.isOk(res, "Did not receive response from server.");
+      assert.property(res, "Cluster", "Expected response to have 'Cluster' property.");
+      assert.isArray(res['Cluster'], "Expected response Cluster property to be an array.");
+      assert.isAbove(res['Cluster'].length, 0, "Request did not return any results.");
+    });
   });
 })
